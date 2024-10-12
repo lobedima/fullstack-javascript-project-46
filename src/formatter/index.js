2ecc8147973275bcc09fccf005207aceae2bad01
Dmitriy Lyubenko <lobedima@gmail.com>
2024-10-12 17:50:04 +0300

    corrections

--- src/formatter/index.js
+++ src/formatter/index.js
@@ -3,14 +3,14 @@ import plain from './plain.js';
 import json from './json.js';
 
 export default (data, formatName) => {
-  if (formatName === 'stylish') {
-    return stylish(data);
+  switch (formatName) {
+    case 'stylish':
+      return stylish(data, 0);
+    case 'json':
+      return json(data);
+    case 'plain':
+      return plain(data);
+    default:
+      throw new Error(`${formatName} is not supported`);
   }
-  if (formatName === 'plain') {
-    return plain(data);
-  }
-  if (formatName === 'json') {
-    return json(data);
-  }
-  return 'Unknown format';
 };
